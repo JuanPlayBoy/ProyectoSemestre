03a77182f27c7a17ec75315992c6054adc7f6b2c
JuanPlayBoy <juanpabloruiz.ec@gmail.com>
2023-05-17 00:06:58 -0500

    Front Terminado

--- reminder/forms.py
+++ reminder/forms.py
@@ -29,6 +29,42 @@ class FormularioReminder(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.fields['invitado'].queryset = invitado_choices
 
+class FormularioReminderEntrada(forms.ModelForm):
+    invitados = forms.ModelMultipleChoiceField(
+    queryset=Invitado.objects.none(),
+    widget=forms.SelectMultiple(attrs={'class': 'form-control', 'name': 'invitados'}),  # Agregamos el atributo 'name'
+    required=False,
+)
+
+    select_all = forms.BooleanField(required=False, initial=False, label='Seleccionar todos')
+
+    class Meta:
+        model = Reminder
+        fields = ['asunto', 'descripcion', 'invitados']
+        widgets = {
+            'asunto': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Escriba el asunto de su notificación'}),
+            'descripcion': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Escriba la descripción de su notificación'}),
+        }
+        labels = {
+            'asunto': 'Título',
+            'descripcion': 'Descripción',
+            'invitados': 'Para',
+        }
+
+    def __init__(self, *args, **kwargs):
+        invitado_choices = kwargs.pop('invitado_choices', [])
+        super().__init__(*args, **kwargs)
+        self.fields['invitados'].queryset = invitado_choices
+
+    def clean(self):
+        cleaned_data = super().clean()
+        select_all = cleaned_data.get('select_all')
+        invitados = cleaned_data.get('invitados')
+        if select_all and invitados:
+            raise forms.ValidationError("No puede seleccionar todos los invitados y elegir invitados individuales al mismo tiempo.")
+        return cleaned_data
+
+
 
 
 
--- reminder/views.py
+++ reminder/views.py
@@ -1,6 +1,7 @@
 
 from django.shortcuts import get_object_or_404, render, redirect
 from .forms import FormularioReminder
+from .forms import FormularioReminderEntrada
 from django.contrib.auth.decorators import login_required
 from .models import Evento
 from .models import Reminder
@@ -25,12 +26,12 @@ def reminder_crear(request, evento_id):
     evento = get_object_or_404(Evento, id=evento_id)
 
     if request.method == 'POST':
-        form = FormularioReminder(request.POST, invitado_choices=evento.invitado_set.all())
+        form = FormularioReminderEntrada(request.POST, invitado_choices=evento.invitado_set.all())
         if form.is_valid():
             reminder = form.save(commit=False)
             reminder.user = request.user
             reminder.evento = evento
-            invitados = form.cleaned_data['invitado']
+            invitados = form.cleaned_data['invitados']
             select_all = form.cleaned_data['select_all']
             if select_all:
                 invitados = evento.invitado_set.all()
@@ -38,7 +39,7 @@ def reminder_crear(request, evento_id):
             reminder.invitado.set(invitados)
             return redirect(reverse('reminder_detail', args=[reminder.id, evento_id]), evento_id=evento_id)
     else:
-        form = FormularioReminder(invitado_choices=evento.invitado_set.all())
+        form = FormularioReminderEntrada(invitado_choices=evento.invitado_set.all())
 
     return render(request, 'reminder_crear.html', {
         'form': form,
@@ -54,26 +55,45 @@ def reminder_detail(request, reminder_id, evento_id):
     if request.method == 'GET':
         invitados = Invitado.objects.filter(reminder=reminder)
         invitado_choices = evento.invitado_set.all()
-        form = FormularioReminder(instance=reminder, invitado_choices=invitado_choices)
+        form = FormularioReminderEntrada(instance=reminder, invitado_choices=invitado_choices)
         return render(request, 'reminder_detail.html', {'reminder': reminder, 'form': form, 'evento': evento, 'invitados': invitados})
     else:
-        form = FormularioReminder(request.POST, instance=reminder, invitado_choices=evento.invitado_set.all())
+        form = FormularioReminderEntrada(request.POST, instance=reminder, invitado_choices=evento.invitado_set.all())
         if form.is_valid():
             reminder = form.save(commit=False)
             reminder.user = request.user
             reminder.evento = evento
             reminder.save()
             form.save_m2m()
-            return redirect(reverse('reminder_detail', args=[reminder.id, evento_id]), evento_id=evento_id)
+
+            # Obtener los invitados seleccionados en el formulario
+            invitados_seleccionados = form.cleaned_data['invitados']
+            select_all = form.cleaned_data['select_all']
+            
+            # Obtener los invitados actuales del reminder
+            invitados_actuales = reminder.invitado.all()
+
+            # Agregar los invitados seleccionados al conjunto existente
+            for invitado in invitados_seleccionados:
+                if select_all or invitado not in invitados_actuales:
+                    reminder.invitado.add(invitado)
+                else:
+                    reminder.invitado.remove(invitado)
+
+            return redirect(reverse('reminder_detail', args=[reminder.id, evento_id]))
         else:
             invitados = Invitado.objects.filter(reminder=reminder)
             return render(request, 'reminder_detail.html', {
                 'reminder': reminder,
                 'form': form,
-                'error': 'Error actualizando la notifiación',
+                'error': 'Error actualizando la notificación',
                 'evento': evento,
                 'invitados': invitados,
             })
+
+
+
+
 
 
 
